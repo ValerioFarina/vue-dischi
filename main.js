8af94c081cf1f44c2c1bb397f43cfc71b08ef87d
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-23 18:37:58 +0100

    Aggiunta la possibilità, tramite una nuova select, di ordinare gli album sia dal più vecchio al più nuovo, sia dal più nuovo al più vecchio

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ var app = new Vue({
     data: {
         albums : [],
         genres : [],
-        selectedGenre : 'All'
+        selectedGenre : 'All',
+        order : 'asc'
     },
 
     methods: {
-
+        // this function sorts the array of albums according to the value of "order"
+        sortAlbums() {
+            if (this.order == 'asc') {
+                // if order is equal to "asc"
+                // then the array is sorted from the oldest album to the newest
+                this.albums.sort((a, b) => parseInt(a.year) - parseInt(b.year));
+            } else if (this.order == 'desc') {
+                // if order is equal to "desc"
+                // then the array is sorted from the newest album to the oldest
+                this.albums.sort((a, b) => parseInt(b.year) - parseInt(a.year));
+            }
+        }
     },
 
     mounted() {
@@ -24,7 +36,7 @@ var app = new Vue({
                     }
                 });
                 // we sort the array of albums from the oldest album to the newest
-                this.albums.sort((a, b) => parseInt(a.year) - parseInt(b.year));
+                this.sortAlbums();
             });
     }
 });
