aa329e1b432803f514413df815ed8cddf09c4b18
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-23 15:53:55 +0100

    Recuperati i generi degli album

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#root',
 
     data: {
-        albums : []
+        albums : [],
+        genres : []
     },
 
     methods: {
@@ -13,7 +14,16 @@ var app = new Vue({
         axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((responseObject) => {
+                // we get from the API the array of albums
                 this.albums = responseObject.data.response;
+                // then, we get all the different genres of the albums
+                this.albums.forEach((album) => {
+                    if (!this.genres.includes(album.genre)) {
+                        this.genres.push(album.genre);
+                    }
+                });
+                console.log(this.genres);
+
             });
     }
 });
